c5869a0a56640d97f216b81aad395b2421405d68
denidomis <80945411+denidomis@users.noreply.github.com>
2024-01-12 11:28:28 +0200

    Friday challenge - Tip calculator (Simona K., Aušra Z., Ieva R., Domantas D., Rolandas Z.)

---
+++ Friday_Challenge/script.js
@@ -0,0 +1,35 @@
+const billAmount = document.getElementById("bill");
+const tipPercentage = document.getElementById("tip");
+const calculate = document.getElementById("calculate");
+const finalTip = document.getElementById("finalTip");
+const total = document.getElementById("total");
+const tipAmount = 0;
+const totalAmount = 0;
+
+calculate.addEventListener("click", () => {
+  event.preventDefault();
+  showImage();
+  const bill = +billAmount.value;
+  const tip = +tipPercentage.value;
+  const tipAmount = (bill * tip) / 100;
+  const totalAmount = bill + tipAmount;
+  finalTip.innerHTML = tipAmount;
+  total.innerHTML = totalAmount;
+});
+
+function showImage() {
+  const image = document.querySelector(".image-div");
+  const image2 = document.querySelector(".image-div2");
+  const image3 = document.querySelector(".image-div3");
+  setTimeout(() => {
+    image.style.display = "block";
+  }, 0);
+  setTimeout(() => {
+    image.style.display = "none";
+    image2.style.display = "block";
+  }, 500);
+  setTimeout(() => {
+    image2.style.display = "none";
+    image3.style.display = "block";
+  }, 1000);
+}
